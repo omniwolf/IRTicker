fd9d4261672683da60655642d66b6ebc7c4c6206
Nick F <seventh_adept@hotmail.com>
2024-12-19 23:16:48 +1100

    decided to build a whole new Form for CB. started.

---
+++ IRTicker/CBAccountsForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRTicker {
+    public partial class CBAccountsForm : Form {
+        public CBAccountsForm() {
+            InitializeComponent();
+
+            // Need to start Coinbase websockets.
+        }
+    }
+}
